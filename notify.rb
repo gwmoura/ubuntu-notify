52f55168e495ef5d2f028f4d8159de425427e95a
George Moura <gwmoura@gmail.com>
2016-04-06 10:58:09 -0300

    organize the code

--- notify.rb
+++ notify.rb
@@ -7,7 +7,9 @@ require 'nokogiri'
 OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 loop do
-  doc = Nokogiri::HTML(open("https://sandbox.pagseguro.uol.com.br/"))
+  url = "https://sandbox.pagseguro.uol.com.br/"
+  puts "checking #{url} - #{Time.now}"
+  doc = Nokogiri::HTML(open(url))
   body_text = doc.css("body").text
   if !body_text.include?("No momento o ambiente de testes do PagSeguro está indisponível")
     Libnotify.show :summary => "#{Time.now}", :body => "Pagseguro sandbox disponível!!! :D", :timeout => 5
